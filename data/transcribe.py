571b5b3ef9d52113f39e4a2ecdc87b5cd588449f
Kyle Smith <kyle.smith@upstart.com>
2020-05-08 16:30:10 -0700

    Object orient things

--- data/transcribe.py
+++ data/transcribe.py
@@ -1,5 +1,6 @@
 import pathlib
 from pathlib import Path
+from typing import Optional
 
 import cv2
 from PIL import Image
@@ -26,67 +27,94 @@ def download_videos():
         ydl.download(video_links)
 
 
-def is_end_of_snippet(text, prev_text):
-    return len(text) < len(prev_text)
+class Snippet:
+    def __init__(self, start_frame: int, start_im: Image):
+        self.start_frame = start_frame
+        self.start_im = start_im
+        self.is_done = False
+        self.last_text_append: Optional[int] = None
+        self.last_text_append_im: Optional[Image] = None
+        self.text = ""
+
+    
+    def process_frame(self, im: Image, frame_num: int):
+        frame_text = Snippet.get_text(im)
+        if len(frame_text) > len(self.text):
+            self.last_text_append = frame_num
+            self.last_text_append_im = im
+            self.text = frame_text
+        elif self.is_end(im, frame_text):
+            self.dump()
+            self.is_done = True
 
 
-def frame_to_seconds(frame):
-    return frame / FRAMES_PER_SECOND
+    def dump(self) -> None:
+        if self.last_text_append is None or self.last_text_append_im is None:
+            raise ValueError("Trying to log without capturing text")
+
+        char = Snippet.get_character(self.start_im)
+        start_timestamp = self.start_frame / FRAMES_PER_SECOND
+        end_timestamp = self.last_text_append / FRAMES_PER_SECOND
+        print(f"{char} {start_timestamp} - {end_timestamp}: {self.text}")
+
+
+    def is_end(self, frame_im: Image, frame_text: str):
+        is_not_dialog = Snippet.get_character(frame_im) is None
+        is_next_dialog = len(frame_text) == 0 and len(self.text) > 0 
+        return is_not_dialog or is_next_dialog
+
+
+    @staticmethod
+    def is_start(im: Image) -> bool:
+        """
+        Checks whether the image is the start of some dialog
+        """
+        has_character = Snippet.get_character(im) is not None
+        has_text = Snippet.get_text(im) != ""
+        return has_character and not has_text
+        
+    @staticmethod
+    def get_character(im) -> Optional[str]:
+        """
+        Crops the image to where the character name would appear
+        and checks whether the characters match any character names
+        """
+        name_box = (360, 614, 655, 695)
+        text = pytesseract.image_to_string(im.crop(box=name_box))
+
+        for c in characters:
+            if c in text:
+                return c
+        return None
+    
+    @staticmethod
+    def get_text(im) -> str:
+        """
+        Crops an image to where the dialog appears and converts to string
+        """
+        name_box = (380, 720, 1541, 967)
+        return pytesseract.image_to_string(im.crop(box=name_box)).strip()
 
 
 def process_video(path):
     cap = cv2.VideoCapture(str(path))
-    currentFrame = 0
-    start_and_stop = None
-    prev_text = ""
-    im_cache = dict()
+    current_frame = 0
 
     while True:
-        currentFrame += 1
+        current_frame += 1
         ret, frame = cap.read()
-        if currentFrame % SKIP_FRAMES != 0:
+        if current_frame % SKIP_FRAMES != 0:
             continue
 
         im = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-        im_cache[currentFrame] = im
-
-        char = get_character(im)
-        if char is not None:
-            if start_and_stop is None:
-                start_and_stop = [currentFrame - SKIP_FRAMES, None]
-
-            text = get_text(im)
-
-            if is_end_of_snippet(text, prev_text):
-                start_and_stop[1] = currentFrame
-                log_snippet(char, prev_text, start_and_stop, im_cache)
-                start_and_stop = None
-
-            prev_text = text
-
-
-def log_snippet(char, text, start_and_stop, image_cache):
-    print(
-        f"{char} {start_and_stop[0] / FRAMES_PER_SECOND} - {start_and_stop[1] / FRAMES_PER_SECOND}: {text}"
-    )
-    image_cache[start_and_stop[0]].save(str(FRAMES_DIR / f"{start_and_stop[0]}.png"))
-    image_cache[start_and_stop[1]].save(str(FRAMES_DIR / f"{start_and_stop[1]}.png"))
-
-
-def get_character(im):
-    name_box = (360, 614, 655, 695)
-    text = pytesseract.image_to_string(im.crop(box=name_box))
-
-    for c in characters:
-        if c in text:
-            return c
-    return None
-
-
-def get_text(im):
-    name_box = (380, 720, 1541, 967)
-    return pytesseract.image_to_string(im.crop(box=name_box)).strip()
+        if Snippet.is_start(im):
+            s = Snippet(current_frame, im)
+            while not s.is_done:
+                current_frame += 1
+                ret, frame = cap.read()
+                im = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+                s.process_frame(im, current_frame)
 
 
 process_video(VIDEOS_DIR / "1.mp4")
