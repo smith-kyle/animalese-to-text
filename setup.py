0c3a2e7f4ad2d11a2d472e3caed758f9ee665aff
Kyle Smith <kyle.smith@upstart.com>
2021-02-08 14:33:36 -0800

    Only add a2t to the package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="a2t",
     use_scm_version=True,
-    packages=find_packages(".", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    packages=['a2t'],
     setup_requires=["setuptools_scm"],
     install_requires=[],
     python_requires=">=3.7.0",
